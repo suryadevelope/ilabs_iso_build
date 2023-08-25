9a956d151142e7aaa440bc0c8584f520cc686767
suryadevelope <suryaprakashlokula@gmail.com>
2023-08-25 18:06:57 +0530

    test72

--- external/libvmc/build-package.sh
+++ external/libvmc/build-package.sh
@@ -714,7 +714,7 @@ while (($# > 0)); do
 
 		
 		ls
-		# mkdir -p "$PKG_CONFIG_LIBDIR"
+		mkdir -p "$PKG_CONFIG_LIBDIR"
 		echo "surya test11 $PKG_CONFIG_LIBDIR $PKG_CONFIG"
 		cat > "$PKG_CONFIG" <<-HERE
 			#!/bin/sh
