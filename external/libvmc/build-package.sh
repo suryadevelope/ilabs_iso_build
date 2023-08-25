b4422334c3d00bb8967c297d819473a75356d0a3
suryadevelope <suryaprakashlokula@gmail.com>
2023-08-25 15:13:18 +0530

    test43

--- external/libvmc/build-package.sh
+++ external/libvmc/build-package.sh
@@ -605,7 +605,7 @@ while (($# > 0)); do
 		source "$PACKAGE_BUILDER_SCRIPT"
 
 		if [ "$CONFIG_BUILDER_SKIP_DEPCHECK" != "true" ]; then
-			echo "sudo chmod 755 $BUILDER_SCRIPTDIR/scripts/buildorder.py"
+			echo "ls -l $BUILDER_SCRIPTDIR/scripts/buildorder.py && sudo +x $BUILDER_SCRIPTDIR/scripts/buildorder.py"
 			while read -r dep; do
 				echo "Building dependency $dep if necessary..."
 				"$BUILDER_SCRIPTDIR/build-package.sh" -a "$PACKAGE_TARGET_ARCH" -s "$dep"
