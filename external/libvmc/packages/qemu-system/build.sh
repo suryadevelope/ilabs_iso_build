9519995a448fb0f39d745199024a917fab442b62
suryadevelope <suryaprakashlokula@gmail.com>
2023-08-29 23:19:49 +0530

    test119

--- external/libvmc/packages/qemu-system/build.sh
+++ external/libvmc/packages/qemu-system/build.sh
@@ -16,6 +16,7 @@ builder_step_configure() {
 	# flags already passed by build scripts but we do not want to
 	# override them with what QEMU configure provides.
 	echo "surya 1111 $PACKAGE_INSTALL_PREFIX ${PACKAGE_TARGET_PLATFORM}"
+	which "${PACKAGE_INSTALL_PREFIX}"
 	chmod +x "${PACKAGE_INSTALL_PREFIX}"
 	chmod +x "${PACKAGE_TARGET_PLATFORM}"
 	ls
