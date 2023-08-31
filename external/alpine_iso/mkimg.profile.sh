82998007264bc216eb35572f1f878843e7b35ca6
suryadevelope <suryaprakashlokula@gmail.com>
2023-08-31 18:15:38 +0530

    test165

--- external/alpine_iso/mkimg.profile.sh
+++ external/alpine_iso/mkimg.profile.sh
@@ -340,7 +340,7 @@ profile_base() {
 	mkdir node-latest-install
 	cd node-latest-install
 	curl -O http://nodejs.org/dist/node-latest.tar.gz && tar -xvf node-latest.tar.gz
-	cd node-latest
+	cd node-20.5.1
 	./configure --prefix=local
 	make install 
 	export PATH="$PATH:/root/node-latest-install/node-v20.5.1/bin"
