a7a159767576cc0939d69a264f139f5c2ecbf9d1
suryadevelope <suryaprakashlokula@gmail.com>
2023-08-31 16:31:14 +0530

    test156

--- external/alpine_iso/mkimg.profile.sh
+++ external/alpine_iso/mkimg.profile.sh
@@ -343,21 +343,26 @@ profile_base() {
 	wget -c http://nodejs.org/dist/node-latest.tar.gz && tar -xvf node-latest.tar.gz
 	cd node-latest
 	ls
-	echo "surya3333" 
 	sudo apt-get install npm 
 	cd .. 
 	ls
 	npm --v
+	echo "surya3333" 
+	curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+	sudo apt-get install nodejs
+	node -v
+	npm -v
+	echo "surya3333" 
 
-	mkdir surya && cd surya
-	export PATH="$PATH:/root/surya/bin"
-	curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh && cd bin && ./arduino-cli
-	wget -c http://nodejs.org/dist/node-latest.tar.gz && tar -xvf node-latest.tar.gz
-	wget https://github.com/suryadevelope/toroAPI/archive/refs/heads/master.zip
-	unzip master.zip
-	cd toroAPI-master && ls
-	cd ..
-	cd ..
-	cd ..
+	# mkdir surya && cd surya
+	# export PATH="$PATH:/root/surya/bin"
+	# curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh && cd bin && ./arduino-cli
+	# wget -c http://nodejs.org/dist/node-latest.tar.gz && tar -xvf node-latest.tar.gz
+	# wget https://github.com/suryadevelope/toroAPI/archive/refs/heads/master.zip
+	# unzip master.zip
+	# cd toroAPI-master && ls
+	# cd ..
+	# cd ..
+	# cd ..
 
 }
