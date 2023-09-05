8c8bd24d077ae71c199f43e274b6d53d2939d94c
suryadevelope <suryaprakashlokula@gmail.com>
2023-09-05 17:06:23 +0530

    test187

--- external/alpine_iso/genapkovl.sh
+++ external/alpine_iso/genapkovl.sh
@@ -267,7 +267,7 @@ EOF
 # ff02::2     ip6-allrouters
 # ff02::3     ip6-allhosts
 # EOF
-
+apk add bridge hostapd wireless-tools wpa_supplicant
 ## Network interface configuration
 mkdir -p "$tmp"/etc/network
 makefile root:root 0644 "$tmp"/etc/network/interfaces <<EOF
@@ -277,8 +277,45 @@ iface lo inet loopback
 auto eth0
 iface eth0 inet dhcp
 
+auto br0
+iface br0 inet dhcp
+  hostname alpine
+  bridge-ports eth0
 
 EOF
+
+mkdir -p "$tmp"/etc/hostapd
+makefile root:root 0644 "$tmp"/etc/hostapd/hostapd.wpa_psk <<EOF
+00:00:00:00:00:00 PASSPHRASE
+EOF
+
+mkdir -p "$tmp"/etc/hostapd
+makefile root:root 0644 "$tmp"/etc/hostapd/hostapd.conf <<EOF
+interface=wlan0
+bridge=br0
+driver=hostap
+logger_syslog=-1
+logger_syslog_level=2
+logger_stdout=-1
+logger_stdout_level=2
+debug=0
+dump_file=/tmp/hostapd.dump
+ctrl_interface=/var/run/hostapd
+ctrl_interface_group=0
+ssid=SecureSSID
+#macaddr_acl=1
+#accept_mac_file=/etc/hostapd/accept
+auth_algs=3
+eapol_key_index_workaround=0
+eap_server=0
+wpa=3
+wpa_psk_file=/etc/hostapd/hostapd.wpa_psk
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=CCMP
+
+EOF
+
+/etc/init.d/hostapd start
 # makefile root:root 0644 "$tmp"/etc/resolv.conf <<EOF
 # nameserver 8.8.8.8
 # nameserver 10.0.2.3
