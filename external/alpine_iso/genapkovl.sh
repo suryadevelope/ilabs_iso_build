6b44b700845b7e19596d39c083f48678f3337428
suryadevelope <suryaprakashlokula@gmail.com>
2023-10-16 13:15:42 +0530

    test219

--- external/alpine_iso/genapkovl.sh
+++ external/alpine_iso/genapkovl.sh
@@ -508,16 +508,17 @@ echo "surya code modified"
 ls
 mkdir surya 
 cd surya
-export PATH="$PATH:arduino-cli" 
+
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh 
 cd bin
 df -h
 find . -name "arduino-cli"
-arduino-cli config init --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json 
-arduino-cli core update-index 
-arduino-cli core install esp8266:esp8266
-arduino-cli sketch new buildino 
-arduino-cli compile -b esp8266:esp8266:nodemcuv2 buildino/buildino.ino --verbose
+export PATH="$PATH:arduino-cli" 
+./arduino-cli config init --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json 
+./arduino-cli core update-index 
+./arduino-cli core install esp8266:esp8266
+./arduino-cli sketch new buildino 
+./arduino-cli compile -b esp8266:esp8266:nodemcuv2 buildino/buildino.ino --verbose
 
 
 ##############################################################################
