80584cb3bfd52373e627058a1f5fab5a847a3249
suryadevelope <suryaprakashlokula@gmail.com>
2023-10-16 17:36:23 +0530

    test228

--- external/alpine_iso/genapkovl.sh
+++ external/alpine_iso/genapkovl.sh
@@ -512,11 +512,11 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 df -h
 find . -name "arduino-cli"
 export PATH="$PATH:/bin/arduino-cli" 
-./arduino-cli config init --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json 
-./arduino-cli core update-index
-./arduino-cli core install esp8266:esp8266
-./arduino-cli sketch new buildino 
-./arduino-cli compile -b esp8266:esp8266:nodemcuv2 buildino/buildino.ino --verbose
+arduino-cli config init --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json 
+arduino-cli core update-index
+arduino-cli core install esp8266:esp8266
+arduino-cli sketch new buildino 
+arduino-cli compile -b esp8266:esp8266:nodemcuv2 buildino/buildino.ino --verbose
 
 ##############################################################################
 ##
