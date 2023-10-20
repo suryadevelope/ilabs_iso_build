224cbf4370e1138cb1f4445031cedcd46e7d3454
suryadevelope <suryaprakashlokula@gmail.com>
2023-10-21 01:08:18 +0530

    test247

--- external/alpine_iso/mkimg.profile.sh
+++ external/alpine_iso/mkimg.profile.sh
@@ -336,21 +336,21 @@ profile_base() {
 	## /root/surya/bin
 	##
 	##############################################################################
-	echo "surya code modified"
-	ls
-	curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh 
-	cd bin
-	ls
-	df -h
-	find . -name "arduino-cli"
-	export PATH="$PATH:arduino-cli" 
-	./arduino-cli config init --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json 
-	./arduino-cli core update-index
-	./arduino-cli core install esp8266:esp8266
-	./arduino-cli sketch new buildino
-	./arduino-cli compile -b esp8266:esp8266:nodemcuv2 buildino/buildino.ino --verbose
+	# echo "surya code modified"
+	# ls
+	# curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh 
+	# cd bin
+	# ls
+	# df -h
+	# find . -name "arduino-cli"
+	# export PATH="$PATH:arduino-cli" 
+	# ./arduino-cli config init --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json 
+	# ./arduino-cli core update-index
+	# ./arduino-cli core install esp8266:esp8266
+	# ./arduino-cli sketch new buildino
+	# ./arduino-cli compile -b esp8266:esp8266:nodemcuv2 buildino/buildino.ino --verbose
 
-	cd ..
-	
+	# cd ..
+
 
 }
